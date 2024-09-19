c833c970e3e9cb61ca1c1a3a38c913990a709a58
Oscar Brunet <oscar.brunet@telecom-sudparis.eu>
2024-09-19 15:11:46 +0200

    Ajout de Training_data_processing.py depuis master

--- Training_data_processing.py
+++ Training_data_processing.py
@@ -20,7 +20,6 @@ df = pd.read_csv("train.csv", encoding="ISO-8859-1")
 # Supprimer les colonnes 1, 4, 5, 6, 7, 8, 9
 df = df.drop(df.columns[[1, 4, 5, 6, 7, 8, 9]], axis=1)
 
-
 def remove_URL(text):
     if isinstance(text, str):
         url = re.compile(r"https?://\S+|www\.\S+")
@@ -65,18 +64,9 @@ for text in df["selected_text"]:
 #itère sur la colonne sentiment de df, si la valeur est negative, on la remplace par 0, si c'est neutral on la remplace par 1 et si c'est positive on la remplace par 2
 df["sentiment"] = df["sentiment"].map({"negative": 0, "neutral": 1, "positive": 2})
 
-train_size = int(df.shape[0] * 0.8)
-
-train_df = df[:train_size]
-val_df = df[train_size:]
-
-train_sentences = train_df.selected_text.to_numpy()
+train_sentences = df.selected_text.to_numpy()
 train_sentences = [str(sentence) for sentence in train_sentences]
-train_labels = train_df.sentiment.to_numpy()
-
-val_sentences = val_df.selected_text.to_numpy()
-val_sentences = [str(sentence) for sentence in val_sentences]
-val_labels = val_df.sentiment.to_numpy()
+train_labels = df.sentiment.to_numpy()
 
 num_unique_words = len(global_counter)
 # Ensure all elements in train_sentences are strings
@@ -86,17 +76,12 @@ tokenizer = Tokenizer(num_words=num_unique_words)
 tokenizer.fit_on_texts(train_sentences)
 word_index = tokenizer.word_index
 train_sequences = tokenizer.texts_to_sequences(train_sentences)
-val_sequences = tokenizer.texts_to_sequences(val_sentences)
 
 max_length = 20
 
 train_padded = pad_sequences(train_sequences, maxlen=max_length, padding="post", truncating="post")
-val_padded = pad_sequences(val_sequences, maxlen=max_length, padding="post", truncating="post")
-
 np.save('train_padded.npy', train_padded)
 np.save('train_labels.npy', train_labels)
-np.save('test_padded.npy', val_padded)
-np.save('test_labels.npy', val_labels)
 
 with open('model_params.txt', 'w') as f:
     f.write(f'max_length: {max_length}\n')
